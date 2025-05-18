0d21a2951697eef34a6237e2fd66110a01b81781
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-05-18 19:43:02 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,4 @@
 apt-get update
 apt-get install -y curl tesseract-ocr tesseract-ocr-eng tesseract-ocr-hrv
 
-# Install uv
-curl -Ls https://astral.sh/uv/install.sh | bash
-export PATH="$HOME/.cargo/bin:$PATH"
-
-uv pip install -r requirements.txt
-
 python app.py
